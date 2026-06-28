37fbd56b3772a91f0ff0281e55f54f6d889efea2
Shristi Rai <shristirai.it@gmail.com>
2026-06-28 15:48:34 +0545

    solved op3

---
+++ module2/op3.js
@@ -0,0 +1,13 @@
+//! triangle inquality theorem
+//? sum of any two sides is greater than third side
+
+const validationCheck = (a,b,c)=>{
+    if((a+b>c) && (b+c>a) && (a+c>b)){
+        console.log("true (Valid)");
+    }
+    else
+        console.log("false (Invalid)");
+}
+
+validationCheck(3,4,5);
+validationCheck(1,2,3);
