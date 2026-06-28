6bebb1f00a806d26384c123b6e5a4273c3c895ad
Shristi Rai <shristirai.it@gmail.com>
2026-06-28 15:55:10 +0545

    solved op4

---
+++ module2/op4.js
@@ -0,0 +1,20 @@
+
+
+const triangle = (a,b,c)=>{
+    if((a+b>c) && (b+c>a) && (a+c>b)){
+        if(a === b && b === c){
+        console.log("Equilateral triangle");
+        }
+        else if (a === b || b === c || a === c){
+            console.log("Isosceles triangle");
+        }
+        else if(a ===! b && a ===! c && b ===! c){
+            console.log("Scalene triangle");
+        }
+    }
+    else
+        console.log("false (Invalid)");
+    
+}
+
+triangle(5,5,8);
